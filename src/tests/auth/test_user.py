c6b3506e70fdf1a9e37f5c95beeb10e69bfbe1bd
Азамат Комаев <azamatkomaev15@gmail.com>
2022-11-05 12:06:19 +0300

    test(test_user.py, test_product.py): delete useless fixture.

--- src/tests/auth/test_user.py
+++ src/tests/auth/test_user.py
@@ -2,12 +2,6 @@ import pytest
 from sqlalchemy.orm import Session
 
 
-@pytest.fixture(autouse=True)
-def create_dummy_user(db: Session):
-    """What to do before and after each test."""
-    yield
-
-
 @pytest.mark.user
 def test_invalid_data_creating_user(db: Session):
     assert 0 == 0
